915c5174a63aa43a8cc9b331314fd00ff9c804e3
MapleInSpring <yifeng@resnet-50-64.resnet.ucsb.edu>
2014-02-24 20:09:30 -0800

    Initial Commit

---
+++ ScrollViews/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ScrollViews
+//
+//  Created by Yifeng Hou on 24/2/14.
+//  Copyright (c) 2014 GraceHou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ScrollViews/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ScrollViews
+//
+//  Created by Yifeng Hou on 24/2/14.
+//  Copyright (c) 2014 GraceHou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
